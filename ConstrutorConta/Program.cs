3465fad4aa4fe3c65e1bc08f064b9f6e679586f8
Lucas Vinicius dos Santos <lucassantosvini17@gmail.com>
2026-03-23 14:25:36 -0300

    Aprendendo Construtor

---
+++ ConstrutorConta/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConstrutorConta;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Conta c1 = new Conta();
+        c1.MostrarAtributos();
+
+        Conta c2 = new Conta("Ana");
+        c2.MostrarAtributos();
+
+        Conta c3 = new Conta("Bia", 300);
+        c3.MostrarAtributos();
+
+        Console.WriteLine("Qtde de instâncias? " + Conta.Contador);
+    }
+}
